827af481e04869a121a0621f26798d47ec9c10b5
Joanthan Fisher <jonathan.fshr@gmail.com>
2024-01-19 12:38:14 +0100

    readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# React + Vite
+# NoQ CMS
+
+## Setting up the Project
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
---
+++ src/pages/LandingPageComponent/components/Post.jsx
@@ -0,0 +1,12 @@
+
+const Post = () => {
+    // fetches from db, with getDoc, not docs Get one article.
+    // Create new collection for only post/blog  
+  return (
+    <div>
+      <p>Post comp</p>
+    </div>
+  )
+}
+
+export default Post
