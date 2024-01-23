f5006e8c2c04d12f497a32677e82e22b7c5cbb38
Joanthan Fisher <jonathan.fshr@gmail.com>
2024-01-23 11:13:06 +0100

    nav links

--- src/pages/LandingPageComponent/components/Navbar.jsx
+++ src/pages/LandingPageComponent/components/Navbar.jsx
@@ -1,39 +1,35 @@
 import logo from "../../../assets/images/logo.png";
 import SmallButtonComponent from "./SmallButtonComponent";
-
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 /* todo: nav links from dummydata -> firebase */
 
 const Navbar = () => {
-  const navigator = useNavigate();
-
-  function loginpageHandler() {
-    navigator("/loginCms");
-  }
 
   return (
     <nav className="bg-white w-full mb-4 mt-2">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex ">
-          <div className="flex items-center sm:pr-10">
-            <img className="block h-10 w-auto" src={logo} alt="Logo på NoQ" />
-          </div>
+            <div className="flex items-center sm:pr-10">
+              <Link to="/">
+                    <img className="block h-10 w-auto" src={logo} alt="Logo på NoQ" />
+                </Link>
+            </div>
           <div className="flex justify-between w-3/4 mx-10">
             <div className="hidden sm:ml-6 sm:flex sm:space-x-10 sm:px-10">
               {/* Navigation Links */}
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="text-acc-blue inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
               >
                 Hem
-              </a>
-              <a
-                href="#"
+              </Link>
+              <Link
+                to="#"
                 className="text-acc-blue inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
               >
                 Bli vår partner
-              </a>
+              </Link>
               <a
                 href="#"
                 className="text-acc-blue inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
@@ -53,12 +49,13 @@ const Navbar = () => {
                 Teamet
               </a>
             </div>
-            <div className="flex items-center justify-center">
+            <Link to="/loginCms"
+            className="flex items-center justify-center">
               <SmallButtonComponent
                 title={"Logga in"}
-                onClick={loginpageHandler}
               ></SmallButtonComponent>
-            </div>
+            </Link>
+            
           </div>
         </div>
       </div>
