96a900d700b8ce26de48c6c5cf0d3a4086f7bcbe
Martin Jonsing <martin@jonsing.se>
2024-01-26 10:11:31 +0100

    Debug

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.VITE_REACT_APP_FIREBASE_API,
-    authDomain: process.env.VITE_REACT_APP_FIREBASE_DOMAIN,
-    projectId: process.env.VITE_REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDERID,
-    appId: process.env.VITE_REACT_APP_FIREBASE_APPID,
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDERID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APPID,
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
