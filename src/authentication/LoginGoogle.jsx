70c7cc7b6df863670aaab4a5d99361a17fa613f8
Joanthan Fisher <jonathan.fshr@gmail.com>
2024-01-26 14:05:37 +0100

    login logic. trying to limit access to emails that is in the db, but im able to login anyway. Need help

--- src/authentication/LoginGoogle.jsx
+++ src/authentication/LoginGoogle.jsx
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "../services/firebase.config";
+import { useState,useEffect } from "react";
+import { signInWithPopup, GoogleAuthProvider,signOut  } from "firebase/auth";
+import { auth, db } from "../services/firebase.config";
+import { collection, doc, getDoc } from '@firebase/firestore';
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // Check if a user is already logged in
+    console.log("Checking user authentication state...");
     const unsubscribe = auth.onAuthStateChanged((user) => {
+      console.log("User:", user);
       if (user) {
         console.log("User is already logged in:", user);
         navigate("/cmsDashboard");
@@ -20,11 +24,34 @@ const Login = () => {
 
   const signInWithGoogle = async () => { 
     try {
-      const provider = await new GoogleAuthProvider();
-      await signInWithPopup(auth, provider);
-      navigate("/cmsDashboard");
+      console.log("Attempting Google sign-in...");
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check if the user exists in the Firestore collection
+      console.log('db:', db);
+      const userDocRef = doc(collection(db, 'users'), user.uid);
+      console.log('userDocRef:', userDocRef); 
+      const docSnapshot = await getDoc(userDocRef);
+      console.log('docSnapshot:', docSnapshot);
+
+      if (docSnapshot.exists()) {
+        const userData = docSnapshot.data();
+        // Check if the user has the required role
+        if (userData && (userData.role === 'admin' || userData.role === 'cms-user')) {
+          navigate('/cmsDashboard');
+        } else {
+          setError('User does not have the required role.');
+          await signOut(auth);
+        }
+      } else {
+        setError('User does not exist in the database.');
+        await signOut(auth);
+      }
     } catch (error) {
-      console.error("Login error:", error.message);
+      console.error('Login error:', error.message);
+      setError(error.message);
     }
   };
 
@@ -37,6 +64,7 @@ const Login = () => {
         >
           Sign in with Google
         </button>
+        {error && <p className="text-red-500">{error}</p>}
       </div>
     </div>
   );
