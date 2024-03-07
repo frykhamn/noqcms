6e9b5f94b5b96605471a81dc3f287097b2c264c4
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-03-07 13:28:59 +0100

    Add News component for displaying post on landing page

--- src/pages/LandingPageComponent/LandingPageComponent.jsx
+++ src/pages/LandingPageComponent/LandingPageComponent.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { collection, getDocs } from '@firebase/firestore';
-import { db } from '../../services/firebase.config'; // Import your Firebase config
+import { collection, getDocs } from "@firebase/firestore";
+import { db } from "../../services/firebase.config"; // Import your Firebase config
 
 import BlogPost from "./components/BlogPost";
 import FooterComponent from "./components/FooterComponent";
@@ -9,6 +9,7 @@ import InformationComponent from "./components/InformationComponent";
 import Post from "./components/Post";
 import Team from "./components/Team";
 import Video from "./components/Video";
+import News from "./components/News";
 
 const LandingComponent = () => {
   const [videos, setVideos] = useState([]);
@@ -16,7 +17,7 @@ const LandingComponent = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const videosCollectionRef = collection(db, 'videoLinkCollection');
+        const videosCollectionRef = collection(db, "videoLinkCollection");
         const data = await getDocs(videosCollectionRef);
         const fetchedVideos = data.docs.map((doc) => ({
           ...doc.data(),
@@ -40,19 +41,17 @@ const LandingComponent = () => {
   );
 };
 
-const demoData =
-{
+const demoData = {
   title: "Demo inspelning",
   text: "Från den 15/12 - 2023. Vi presenterar NoQ-gänget och går igenom varför vi gör det här samt vad våra förhoppningar är. Disskutioner om värdefull och nödvändig feedback kring produkten, dess syfte, användningsområde samt vilka problem den kan lösa.",
   // videoSrc: "https://drive.google.com/file/d/1DcezvhiJ3QRuydUTvxG8zZlm9EsrS-Zk/preview"
-  videoSrc: "https://youtu.be/WnBojfIfds8"
+  videoSrc: "https://youtu.be/WnBojfIfds8",
 };
 
-const aboutData =
-{
+const aboutData = {
   title: "Om oss video presentation",
   text: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
-  videoSrc: "https://youtu.be/WnBojfIfds8"
+  videoSrc: "https://youtu.be/WnBojfIfds8",
 };
 
 const Main = ({ videos }) => {
@@ -60,6 +59,7 @@ const Main = ({ videos }) => {
     <>
       <main>
         <InformationComponent></InformationComponent>
+        <News></News>
 
         {videos.map((video) => (
           <Video
@@ -72,7 +72,6 @@ const Main = ({ videos }) => {
         ))}
 
         <Team></Team>
-
       </main>
     </>
   );
